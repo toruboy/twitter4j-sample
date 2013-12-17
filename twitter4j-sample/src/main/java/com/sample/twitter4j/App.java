1939c08b53f06bed256bdf8dc3f6bce6b904d4d4
Toru Watanabe <toru.watanabe0329@gmail.com>
2013-12-18 01:01:34 +0900

    特定のユーザのタイムラインを表示するよう修正

--- twitter4j-sample/src/main/java/com/sample/twitter4j/App.java
+++ twitter4j-sample/src/main/java/com/sample/twitter4j/App.java
@@ -1,24 +1,29 @@
 package com.sample.twitter4j;
 
+import org.apache.commons.lang3.StringUtils;
+
+import twitter4j.Paging;
+import twitter4j.ResponseList;
+import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-import twitter4j.User;
 
 /**
  * Hello world!
  *
  */
-public class App
-{
-    public static void main( String[] args ) throws TwitterException
-    {
-    	Twitter twitter = new TwitterFactory().getInstance();
-        User user = twitter.verifyCredentials();
-
-        System.out.println(user.getName());
-        System.out.println(user.getScreenName());
-        System.out.println(user.getFriendsCount());
-        System.out.println(user.getFollowersCount());
-    }
+public class App {
+	public static void main(String[] args) throws TwitterException {
+		Twitter twitter = new TwitterFactory().getInstance();
+		String screenName = args[0];
+		ResponseList<Status> statuses = twitter.getUserTimeline(screenName,
+				new Paging(1, 200));
+		for (Status status : statuses) {
+			if (!status.isRetweet()
+					&& !StringUtils.contains(status.getText(), "@")) {
+				System.out.println(status.getText());
+			}
+		}
+	}
 }
